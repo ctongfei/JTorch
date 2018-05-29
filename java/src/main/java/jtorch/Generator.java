d1e4e1946afd4876a3a92571fc11bb3aac285e13
ctongfei <ctongfei@gmail.com>
2018-05-28 17:22:40 -0700

    THRandom

--- java/src/main/java/jtorch/Generator.java
+++ java/src/main/java/jtorch/Generator.java
@@ -6,5 +6,29 @@ import jtorch.jni.*;
  * Proxy class for the Torch THGenerator type.
  * @author Tongfei Chen
  */
-public class Generator extends THGenerator {
+public class Generator extends SWIGTYPE_p_THGenerator implements AutoCloseable {
+
+    protected Generator(SWIGTYPE_p_THGenerator underlying) {
+        super(SWIGTYPE_p_THGenerator.getCPtr(underlying), true);
+    }
+
+    protected long cPtr() {
+        return SWIGTYPE_p_THGenerator.getCPtr(this);
+    }
+
+    public Generator() {
+        this(TH.THGenerator_new());
+    }
+
+    // TODO: copy
+
+
+    public void free() {
+        TH.THGenerator_free(this);
+    }
+
+    public void close() {
+        free();
+    }
+
 }
